b95dae083de1401b12f989bc67b1f98643b5b87c
Viogoo <158023320+Viogoo@users.noreply.github.com>
2024-05-08 09:49:55 +0300

    rtwrt

--- market.js
+++ market.js
@@ -9,11 +9,22 @@ let itemsDiv = document.getElementById("items");
 //перевірка існування знайденого блоку
 if (itemsDiv) {
     //вивід знайденого елементу
-    console.log(itemsDiv)
-    console.log('поле classList:', itemsDiv.classList)
-    } else {
+
+  
+
         //вивід повідомлення про незнайдений блок
         console.log('блок товарів не знайдено')
     }
 
 
+//Визначення масиву товарів
+let itemsArray = ['', '', '', '']
+
+console.log(itemsArray)
+
+
+
+// Виведемо в консоль елементів масиву
+for (let i = 0; i < itemsArray.length; i++){
+    console.log(itemsArray[i])
+}
